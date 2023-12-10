07d0513f4c695c36d725a5831806ed74f3f9f375
Elias Symons <eisymonsmusic@gmail.com>
2023-12-10 13:06:31 -0500

    fix width

--- app/pages/hero/page.tsx
+++ app/pages/hero/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 function Hero() {
   return (
-    <div className="w-full flex flex-col items-center justify-center py-16 gap-8 hero">
-      <h1 className="text-5xl font-extrabold max-w-sm text-light  text-center px-4 leading-[4.2rem] sm:text-8xl sm:max-w-[50rem] ">
+    <div className="w-full flex flex-col items-center justify-center py-16 gap-4 hero">
+      <h1 className="text-5xl font-extrabold max-w-sm text-light  text-center px-4 leading-[4.2rem] sm:text-5xl sm:max-w-[50rem] ">
         A place of Love and Caring for all.{" "}
       </h1>
 
